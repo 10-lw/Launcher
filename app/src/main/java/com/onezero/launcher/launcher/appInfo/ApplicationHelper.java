c8eae8072217be68d094296ade82272e1a7d8850
10-lw <sxq5192@dingtalk.com>
2018-09-20 14:39:30 +0800

    update application helper

--- app/src/main/java/com/onezero/launcher/launcher/appInfo/ApplicationHelper.java
+++ app/src/main/java/com/onezero/launcher/launcher/appInfo/ApplicationHelper.java
@@ -1,11 +1,14 @@
 package com.onezero.launcher.launcher.appInfo;
 
 import android.app.AlertDialog;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.util.Log;
 
 import com.onezero.launcher.launcher.R;
@@ -45,6 +48,31 @@ public class ApplicationHelper {
     public static boolean isSystemApp(PackageInfo pInfo) {
         //判断是否是系统软件
         return ((pInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0);
+    }
+
+    public static boolean isSystemApp(Context context, Intent intent) {
+        PackageManager pm = context.getPackageManager();
+        ComponentName cn = intent.getComponent();
+        String packageName = null;
+        if (cn == null) {
+            ResolveInfo info = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            if ((info != null) && (info.activityInfo != null)) {
+                packageName = info.activityInfo.packageName;
+            }
+        } else {
+            packageName = cn.getPackageName();
+        }
+        if (packageName != null) {
+            try {
+                PackageInfo info = pm.getPackageInfo(packageName, 0);
+                return (info != null) && (info.applicationInfo != null) &&
+                        ((info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0);
+            } catch (PackageManager.NameNotFoundException e) {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
     public static boolean isSystemUpdateApp(PackageInfo pInfo) {
